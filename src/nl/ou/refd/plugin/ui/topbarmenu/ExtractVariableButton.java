00ed459bf4c6c6e598f2b2ad1a943cf69214df6f
Danny Faber <danny.faber1982@gmail.com>
2025-06-04 22:49:58 +0200

    Added class ExtractVariableButton as a MenuButton that retrieves selected expression in code

---
+++ src/nl/ou/refd/plugin/ui/topbarmenu/ExtractVariableButton.java
@@ -0,0 +1,58 @@
+package nl.ou.refd.plugin.ui.topbarmenu;
+
+import org.eclipse.core.commands.ExecutionEvent;
+
+import com.ensoftcorp.open.commons.ui.utilities.DisplayUtils;
+
+import nl.ou.refd.exceptions.NoActiveProjectException;
+import nl.ou.refd.locations.graph.GraphQuery;
+import nl.ou.refd.locations.graph.ProgramLocation;
+import nl.ou.refd.locations.graph.SelectionUtil;
+import nl.ou.refd.locations.specifications.ExpressionSpecification;
+import nl.ou.refd.locations.specifications.VariableSpecification;
+import nl.ou.refd.plugin.Controller;
+
+/**
+ * Class representing the menu button for the Extract Variable refactoring
+ * option. The presence of this button can be configured in plugin.xml.
+ */
+public class ExtractVariableButton extends MenuButtonHandler {
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void handle(ExecutionEvent event) {
+		GraphQuery selectedElement = SelectionUtil.getSelection();
+		
+		if (selectedElement.locationCount() < 1) {
+			DisplayUtils.showMessage("Error: No selection made");
+			return;
+		}
+		
+		ProgramLocation location = selectedElement.singleLocation();
+		
+		ExpressionSpecification expressionSource = null;
+		
+		if (ExpressionSpecification.locationIsExpression(location)) {
+			expressionSource = new ExpressionSpecification(location);
+		}
+		else {
+			DisplayUtils.showMessage("Error: Selection was not an expression");
+			return;
+		}
+		
+		//TODO: ask user for a variable name to prepare a VariableSpecification as the destination
+		VariableSpecification destination = null;
+		
+		try {
+			Controller.getController().extractVariable(expressionSource, destination);
+		} catch (NoActiveProjectException e) {
+			DisplayUtils.showMessage("Error: No active project");
+			return;
+		}
+	}
+	
+	
+
+}
